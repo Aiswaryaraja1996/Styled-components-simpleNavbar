6c55f6a9bbb4a45cd1343f0503d7ccb66b855285
Aiswaryaraja1996 <63009970+Aiswaryaraja1996@users.noreply.github.com>
2022-01-09 11:59:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import styled from "styled-components";
+
+const ToolBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.25rem;
+  background-color: #3f51b5;
+  color: white;
+  font-size: 1rem;
+  > * {
+    margin: 0.5rem;
+    &:nth-child(2) {
+      display: flex;
+      gap: 1rem;
+    }
+  }
+`;
+
+const Logo = styled.div`
+  display: flex;
+  > * {
+    &:nth-child(1) {
+      width: 40px;
+    }
+  }
+  @media (max-width: 600px) {
+    display: none;
+  }
+`;
+
+const DrawerItem = ({ label }) => {
+  return <div>{label}</div>;
+};
+
+function Navbar() {
+  return (
+    <ToolBar>
+      <Logo>
+        <img src="./logo.png" alt="logo" />
+        <p>Let's Create</p>
+      </Logo>
+
+      <div>
+        <DrawerItem label="About us" />
+        <DrawerItem label="Prices" />
+        <DrawerItem label="Other" />
+        <DrawerItem label="Contact" />
+      </div>
+    </ToolBar>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
     </div>
   );
 }
